b6026d9d1aea52205f6bfac151268863d864ac86
Kyle Boone <kboone@berkeley.edu>
2021-08-24 13:03:16 -0700

    Implemented PS1 bandpasses in upstream sncosmo, so remove them here

--- parsnip/utils.py
+++ parsnip/utils.py
@@ -269,38 +269,6 @@ def split_train_test(dataset):
     return train_dataset, test_dataset
 
 
-def load_panstarrs_bandpasses():
-    """Download and load the panstarrs bandpasses into sncosmo"""
-    # Figure out where the bandpass data file ended up.
-    # TODO: Download this data if it isn't on disk.
-    dirname = os.path.dirname
-    package_root_directory = dirname(dirname(os.path.abspath(__file__)))
-
-    path = os.path.join(package_root_directory, 'data', 'apj425122t3_mrt.txt')
-
-    # Load the bandpasses into sncosmo
-    from astropy.io import ascii
-
-    band_data = ascii.read(path)
-
-    wave = band_data['Wave'] * 10.
-
-    bands = {
-        'ps1::g': band_data['gp1'],
-        'ps1::r': band_data['rp1'],
-        'ps1::i': band_data['ip1'],
-        'ps1::z': band_data['zp1'],
-    }
-
-    for band_name, band_data in bands.items():
-        band = sncosmo.Bandpass(wave, band_data, name=band_name)
-        sncosmo.registry.register(band, force=True)
-
-
-# Automatically load bandpasses when parsnip is first imported
-load_panstarrs_bandpasses()
-
-
 def frac_to_mag(fractional_difference):
     """Convert a fractional difference to a difference in magnitude.
 
