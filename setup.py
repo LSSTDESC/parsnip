849536d44b1b5c473bae09da548c93b3fe4d9188
Kyle Boone <kboone@berkeley.edu>
2021-09-07 13:56:09 -0700

    Update build system to use PEP 518

--- setup.py
+++ setup.py
@@ -1,15 +1,2 @@
-import os
 from setuptools import setup
-
-setup(
-    name='parsnip',
-    version='0.1',
-    description='ParSNIP: Empirical Generative Models of Astronomical Transients',
-    url='http://github.com/kboone/parsnip',
-    author='Kyle Boone',
-    author_email='kyboone@uw.edu',
-    license='BSD',
-    packages=['parsnip'],
-    data_files=[('data', ['apj425122t3_mrt.txt'])],
-    scripts=['scripts/' + f for f in os.listdir('scripts')],
-)
+setup()
